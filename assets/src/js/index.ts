99e135a5622a91609894d75d529be6698837be0b
mishantrop <quasi-art@yandex.ru>
2023-03-19 23:27:46 +0300

    Some navigation and contacts

--- assets/src/js/index.ts
+++ assets/src/js/index.ts
@@ -1,4 +1,17 @@
 import '../css/main.css'
 
-// eslint-disable-next-line no-console
-console.log('Hello, World')
+const initScrollNavigation = () => {
+    const links = document.querySelectorAll('#header-nav a')
+
+    links.forEach((link) => {
+        link.addEventListener('click', (event) => {
+            console.log(event)
+
+            const section = document.querySelector(`[data-section="${(event.target?.hash || '').replace('#', '')}"]`)
+
+            section?.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' })
+        })
+    })
+}
+
+initScrollNavigation()
