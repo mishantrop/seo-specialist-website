3d1a4b23c8366d7b623842a40013f62c71396ff7
mishantrop <quasi-art@yandex.ru>
2023-03-24 01:30:23 +0300

    Certificates

--- assets/src/js/index.ts
+++ assets/src/js/index.ts
@@ -1,3 +1,5 @@
+import { tns } from 'tiny-slider/src/tiny-slider'
+
 import '../css/main.css'
 
 const initScrollNavigation = () => {
@@ -5,8 +7,6 @@ const initScrollNavigation = () => {
 
     links.forEach((link) => {
         link.addEventListener('click', (event) => {
-            console.log(event)
-
             const section = document.querySelector(`[data-section="${(event.target?.hash || '').replace('#', '')}"]`)
 
             section?.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' })
@@ -14,4 +14,26 @@ const initScrollNavigation = () => {
     })
 }
 
+const initCertsSlider = () => {
+    const slider = tns({
+        container: '.certs-slider',
+        slideBy: 'page',
+        autoplay: false,
+        mouseDrag: true,
+        gutter: 10,
+        responsive: {
+            0: {
+                items: 2,
+                edgePadding: 30,
+            },
+            768: {
+                items: 4,
+                edgePadding: 50,
+            },
+        },
+    })
+    console.log(slider)
+}
+
 initScrollNavigation()
+initCertsSlider()
