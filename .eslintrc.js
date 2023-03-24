a52520e0132b2597b6d98c6591b45316331208fe
mishantrop <quasi-art@yandex.ru>
2023-03-25 02:15:26 +0300

    Markup, js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,26 +4,25 @@ module.exports = {
     plugins: [
         '@typescript-eslint',
     ],
-
-    // env: {
-    //   browser: true,
-    //   es2020: true,
-    // },
+    env: {
+        browser: true,
+        es2020: true,
+    },
     extends: [
         'airbnb-base',
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'standard',
     ],
-    // parserOptions: {
-    //   ecmaVersion: 11,
-    //   sourceType: 'module',
-    // },
+    parserOptions: {
+        ecmaVersion: 11,
+        sourceType: 'module',
+    },
     rules: {
         'array-bracket-spacing': [ 'error', 'always' ],
         'comma-dangle': [ 'error', 'always-multiline' ],
         'space-before-function-paren': [ 'error', 'never' ],
         semi: [ 1, 'never' ],
-        indent: [ 'error', 4 ],
+        indent: [ 'warn', 4 ],
     },
 }
--- assets/src/js/index.ts
+++ assets/src/js/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+import axios from 'axios'
 import { tns } from 'tiny-slider/src/tiny-slider'
 
 import '../css/main.css'
@@ -7,33 +9,99 @@ const initScrollNavigation = () => {
 
     links.forEach((link) => {
         link.addEventListener('click', (event) => {
+            event.preventDefault()
             const section = document.querySelector(`[data-section="${(event.target?.hash || '').replace('#', '')}"]`)
 
             section?.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' })
         })
     })
+
+    const feedbackLink = document.querySelector('#feedback-link')
+    feedbackLink.addEventListener('click', (event) => {
+        event.preventDefault()
+        const section = document.querySelector('[data-section="feedback"]')
+        section?.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' })
+    })
 }
 
 const initCertsSlider = () => {
     const slider = tns({
-        container: '.certs-slider',
-        slideBy: 'page',
         autoplay: false,
-        mouseDrag: true,
+        container: '.certs-slider',
+        controls: false,
         gutter: 10,
+        mouseDrag: true,
+        nav: false,
+        swipeAngle: 35,
+        slideBy: 'page',
         responsive: {
             0: {
+                items: 1,
+                edgePadding: 20,
+            },
+            320: {
+                items: 1,
+                edgePadding: 32,
+            },
+            600: {
                 items: 2,
-                edgePadding: 30,
+                edgePadding: 32,
             },
             768: {
-                items: 4,
-                edgePadding: 50,
+                items: 3,
+                edgePadding: 32,
             },
         },
     })
-    console.log(slider)
+}
+
+const initFeedbackForm = () => {
+    const form: HTMLFormElement | null = document.querySelector('#feedback-form')
+    if (form !== null) {
+        const successElement = document.querySelector('#feedback-success')
+        const errorElement = document.querySelector('#feedback-error')
+
+        form.addEventListener('submit', (event) => {
+            event.preventDefault()
+            const formData = new FormData(form)
+            axios
+                .post(
+                    form.getAttribute('action'),
+                    formData,
+                )
+                .then((response) => {
+                    console.log(response)
+                    successElement.style.display = 'block'
+                    errorElement.style.display = 'none'
+                })
+                .catch((exception: Error) => {
+                    console.error(exception)
+                    successElement.style.display = 'none'
+                    errorElement.style.display = 'block'
+                })
+        })
+    } else {
+        console.warn('Form doesn exists')
+    }
+}
+
+const skillsWidth = () => {
+    const skills = document.querySelectorAll('.skill')
+
+    skills.forEach((skill, idx) => {
+        if (window.matchMedia('(min-width: 769px)').matches) {
+            skill.style.width = [ 0, 1, 2 ].includes(idx % 5) ? '30%' : '46%'
+        } else {
+            skill.style.width = [ 0, 1 ].includes(idx % 3) ? '48%' : '100%'
+        }
+    })
 }
 
 initScrollNavigation()
 initCertsSlider()
+initFeedbackForm()
+skillsWidth()
+
+window.addEventListener('resize', () => {
+    skillsWidth()
+})
